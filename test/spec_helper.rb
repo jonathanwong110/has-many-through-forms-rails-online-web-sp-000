3aa2cca298e6bb0e7e2eadbb412fa6057f40c912
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-09 21:45:21 +0000

    Automatically backed up by Learn

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -8,3 +8,13 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+require "spec_helper"
+
+  describe "Song" do
+  
+  describe "#initialize" do
+  it "accepts a name for the new song" do
+  
+  end
+end
